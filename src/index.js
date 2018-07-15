5c1968386957562e758017753274dbed8bc89e97
valtermartins1301 <valtermartins.1301@gmail.com>
2018-07-15 17:21:14 -0300

    add css reset

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { ThemeProvider } from 'react-jss';
 import App from './views/App/component';
 import registerServiceWorker from './registerServiceWorker';
+import theme from './theme';
+import './index.css';
 
 render(
-  <App />,
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
   document.getElementById('root'),
 );
 registerServiceWorker();
