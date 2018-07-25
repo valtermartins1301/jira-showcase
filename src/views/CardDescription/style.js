1fb35554f19d5e636d8f4f8ff5f0cf30031612bb
valtermartins1301 <valtermartins.1301@gmail.com>
2018-07-24 22:02:44 -0300

    create card description style

--- src/views/CardDescription/style.js
+++ src/views/CardDescription/style.js
@@ -10,6 +10,24 @@ const styles = () => ({
     marginBottom: '45px',
     position: 'relative',
     transition: 'all 0.2s ease-in-out',
+    '*': {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%,-50%)',
+    },
+  },
+  title: {
+    textAlign: 'center',
+    h3: {
+      fontSize: '20px',
+      fontWeight: 'bold',
+      margin: '15px auto',
+      overflow: 'hidden',
+      whiteSpace: 'nowrap',
+      textOverflow: 'ellipsis',
+      width: '100%',
+    },
   },
 });
 
